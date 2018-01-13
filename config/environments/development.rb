0a8b2c101a3973763ecb56aa539bed1a8c4ee2a0
Ilya Sotkov <ilya@sotkov.com>
2018-01-13 16:43:52 +0200

    Use range for whitelisted IPs in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.web_console.whitelisted_ips = '172.22.0.1'
+  config.web_console.whitelisted_ips = '172.16.0.0/12'
 
   # Do not eager load code on boot.
   config.eager_load = false
